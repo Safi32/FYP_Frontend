f75b94c4df55828b5c12c4a3b81f8341e9bea690
Safi32 <138612632+Safi32@users.noreply.github.com>
2024-10-14 13:51:49 +0500

    Colors updated

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color black = Color(0xFF1E1E1E);
+const Color peach = Color(0xFFF1D693);
+const Color yellow = Color(0xFFF6B931);
+const Color orange = Color(0xFFC95E29);
+const Color brown = Color(0xFF5E4228);
