eb500f6bdba2b0c5fc8127ab9af7d5e568added4
Safi32 <138612632+Safi32@users.noreply.github.com>
2024-10-14 14:43:10 +0500

    Curved Navigation Bar

--- lib/view/test_screen.dart
+++ lib/view/test_screen.dart
@@ -1,10 +1,64 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class TestScreen extends StatelessWidget {
+class TestScreen extends StatefulWidget {
   const TestScreen({super.key});
 
   @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text("Home Screen")),
+    const Center(child: Text("Settings Screen")),
+    const Center(child: Text("Account Screen")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Screen"),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        items: <Widget>[
+          _buildNavItem(Icons.home, _selectedIndex == 0 ? "Home" : ""),
+          _buildNavItem(Icons.settings, _selectedIndex == 1 ? "Settings" : ""),
+          _buildNavItem(Icons.account_circle_rounded,
+              _selectedIndex == 2 ? "Account" : ""),
+        ],
+        color: Colors.blue,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: const Duration(milliseconds: 300),
+        index: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 30),
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: const TextStyle(fontSize: 12),
+        ),
+      ],
+    );
   }
 }
