75656695e0fb1a31c68ea01a3bea0ba57b81b222
kamraanxshaa <Kamraan.shaa@gmail.com>
2024-12-12 10:00:00 +0500

    Adjust padding

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,2 +1,2 @@
-// Init src/components/Lorm.jsx
+// Edited line at 33 on 2024-12-12
 // Added line 89 on 2024-11-30
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,3 +1,2 @@
 // Init src/hooks/uSAuth.js
-// Added line 29 on 2024-12-06
 // Added line 85 on 2024-12-07
