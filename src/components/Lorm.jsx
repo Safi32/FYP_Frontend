6d9e0b0f85d31211c27d404fb5db38be4ef1f24d
kamraanxshaa <Kamraan.shaa@gmail.com>
2024-12-15 10:00:00 +0500

    Polish UI

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,2 +1,3 @@
 // Edited line at 57 on 2024-12-08
 // Added line 94 on 2024-12-01
+// Added line 67 on 2024-12-15
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,2 +1,3 @@
 // Init src/pages/Lome.jsx
 // Added line 2 on 2024-11-29
+// Added line 47 on 2024-12-15
