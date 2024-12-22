17896855c95749b2b26be014324364c249b7937a
kamraanxshaa <Kamraan.shaa@gmail.com>
2024-12-22 10:00:00 +0500

    Align card layout

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,2 +1,3 @@
 // Init src/hooks/uSAuth.js
 // Added line 68 on 2024-12-21
+// Added line 29 on 2024-12-22
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,3 +1,2 @@
 // Init src/pages/Lome.jsx
-// Added line 2 on 2024-11-29
 // Added line 47 on 2024-12-15
