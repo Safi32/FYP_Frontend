e731f83aa6654809c65907d551e38c6ab30e89e1
kamraanxshaa <Kamraan.shaa@gmail.com>
2024-12-24 10:00:00 +0500

    Update header style

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,2 +1,2 @@
-// Init src/components/Looter.jsx
+// Edited line at 68 on 2024-12-24
 // Added line 86 on 2024-12-21
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -3,3 +3,4 @@
 // Added line 16 on 2024-12-11
 // Added line 46 on 2024-12-12
 // Added line 69 on 2024-12-16
+// Added line 9 on 2024-12-24
