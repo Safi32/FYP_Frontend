1780b12453a5ea4eb944da6a0b4a7b7121373bcd
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-01-11 10:00:00 +0500

    Align card layout

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,3 +1,4 @@
 // Edited line at 17 on 2024-12-05
 // Added line 72 on 2024-12-03
 // Added line 63 on 2025-01-05
+// Added line 45 on 2025-01-11
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -1,3 +1,2 @@
 // Init src/pages/Hogin.jsx
-// Added line 26 on 2024-12-28
 // Added line 28 on 2025-01-07
