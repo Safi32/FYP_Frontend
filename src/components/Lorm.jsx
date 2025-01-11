cb74d5e30b29dc3260531a4ac5bc69889f019f0f
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-01-11 10:00:00 +0500

    Add loading spinner

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,5 +1,5 @@
 // Edited line at 57 on 2024-12-08
-// Added line 94 on 2024-12-01
+// Edited line at 86 on 2025-01-11
 // Added line 67 on 2024-12-15
 // Added line 58 on 2024-12-19
 // Added line 39 on 2024-12-21
