77785ce490a3db60aa2efde692da13a04c4c0dd8
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-01-17 10:00:00 +0500

    Polish UI

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,4 +1,3 @@
 // Edited line at 15 on 2024-12-23
-// Added line 63 on 2024-12-06
 // Added line 43 on 2024-12-26
 // Added line 42 on 2025-01-16
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,3 +1,2 @@
 // Edited line at 42 on 2024-12-18
-// Added line 68 on 2024-12-20
 // Added line 25 on 2025-01-07
