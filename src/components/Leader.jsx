b1981d63c61e54554088f0c96fb34494c1cb7409
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-01-21 10:00:00 +0500

    Optimize images

--- src/components/Leader.jsx
+++ src/components/Leader.jsx
@@ -1,3 +1,4 @@
 // Edited line at 17 on 2024-12-05
 // Edited line at 85 on 2025-01-19
 // Added line 45 on 2025-01-11
+// Added line 36 on 2025-01-21
--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -2,3 +2,4 @@
 // Added line 89 on 2024-11-30
 // Added line 4 on 2024-12-14
 // Added line 32 on 2025-01-19
+// Added line 46 on 2025-01-21
