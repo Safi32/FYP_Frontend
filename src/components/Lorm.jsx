48a2f57ea69736ccd178e11e797524f1b17b1b52
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-01-27 10:00:00 +0500

    Update dependencies

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -2,5 +2,4 @@
 // Edited line at 86 on 2025-01-11
 // Edited line at 36 on 2025-01-16
 // Added line 58 on 2024-12-19
-// Added line 39 on 2024-12-21
 // Added line 27 on 2025-01-05
--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -1,2 +1,2 @@
-// Init src/pages/Arofile.jsx
+// Edited line at 89 on 2025-01-27
 // Added line 50 on 2025-01-09
