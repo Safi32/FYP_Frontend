ba9b9c343954bbba7fa095e7b5ed31c5f5ba22a0
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-02-06 10:00:00 +0500

    Update header style

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,7 +1,7 @@
 // Edited line at 61 on 2025-01-13
 // Added line 89 on 2024-11-30
 // Added line 4 on 2024-12-14
-// Added line 32 on 2025-01-19
+// Edited line at 57 on 2025-02-06
 // Added line 46 on 2025-01-21
 // Added line 76 on 2025-01-25
 // Added line 75 on 2025-01-29
