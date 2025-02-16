7e4556f8ea159059ec30f6fc763e6ba9183d6877
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-02-16 10:00:00 +0500

    Fix CSS bug

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,4 @@
 // Edited line at 68 on 2024-12-24
 // Edited line at 70 on 2025-02-08
 // Added line 74 on 2025-02-06
+// Added line 35 on 2025-02-16
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -1,4 +1,4 @@
-// Init src/pages/Hogin.jsx
+// Edited line at 52 on 2025-02-16
 // Added line 57 on 2024-12-08
 // Edited line at 89 on 2025-01-31
 // Added line 7 on 2025-01-02
