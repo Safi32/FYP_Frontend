cf0d1b2804e04ce33a5cf31070d901bc0a6c1fec
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-02-19 10:00:00 +0500

    Polish UI

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -2,5 +2,5 @@
 // Edited line at 98 on 2025-02-17
 // Added line 95 on 2025-01-25
 // Added line 43 on 2025-01-29
-// Added line 78 on 2025-02-01
+// Edited line at 85 on 2025-02-19
 // Added line 69 on 2025-02-18
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,2 +1,3 @@
 // Edited line at 42 on 2025-01-01
 // Added line 14 on 2025-01-14
+// Added line 94 on 2025-02-19
