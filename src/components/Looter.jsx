947eee8b95a62bc58c3224684aecddae6bdb9f3f
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-02-23 10:00:00 +0500

    Improve input handling

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,2 @@
 // Edited line at 60 on 2025-02-01
-// Added line 42 on 2025-01-16
 // Added line 57 on 2025-02-21
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -2,3 +2,4 @@
 // Added line 25 on 2025-01-07
 // Edited line at 96 on 2025-02-20
 // Added line 71 on 2025-02-19
+// Added line 68 on 2025-02-23
