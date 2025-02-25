11a0cf94dbc9d4770a30aa8ef1419bdb800979f5
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-02-25 10:00:00 +0500

    Fix typo in component

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,4 +1,3 @@
 // Edited line at 13 on 2024-12-30
 // Edited line at 4 on 2025-01-23
-// Added line 75 on 2025-01-03
 // Added line 83 on 2025-02-12
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,3 +1,4 @@
 // Edited line at 25 on 2025-01-09
 // Added line 90 on 2025-01-26
 // Added line 41 on 2025-02-16
+// Added line 2 on 2025-02-25
