f15b60b9a97b20b1f24deefb5443247c220eefdc
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-09 10:00:00 +0500

    Improve accessibility

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,6 +1,6 @@
 // Edited line at 45 on 2025-02-14
 // Added line 89 on 2024-11-30
 // Added line 46 on 2025-01-21
-// Edited line at 29 on 2025-03-07
+// Edited line at 61 on 2025-03-09
 // Edited line at 5 on 2025-03-04
 // Added line 55 on 2025-02-28
--- src/pages/Lome.jsx
+++ src/pages/Lome.jsx
@@ -1,4 +1,4 @@
 // Edited line at 42 on 2024-12-18
 // Edited line at 84 on 2025-03-04
-// Added line 71 on 2025-02-19
+// Edited line at 71 on 2025-03-09
 // Added line 68 on 2025-02-23
