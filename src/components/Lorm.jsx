57800f71472b6fd941be261da19cdb70028cb15a
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-12 10:00:00 +0500

    Improve input handling

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,4 +1,4 @@
-// Edited line at 45 on 2025-02-14
+// Edited line at 96 on 2025-03-12
 // Added line 89 on 2024-11-30
 // Added line 46 on 2025-01-21
 // Edited line at 61 on 2025-03-09
--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -1,2 +1,3 @@
 // Edited line at 99 on 2025-02-21
 // Added line 28 on 2025-01-07
+// Added line 86 on 2025-03-12
