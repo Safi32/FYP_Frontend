9ad1d3983c32a9eba98b3635f71a8b55fd57e8b2
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-15 10:00:00 +0500

    Add mobile nav support

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,5 +1,5 @@
 // Init src/hooks/uSAuth.js
 // Added line 95 on 2025-01-25
 // Added line 43 on 2025-01-29
-// Edited line at 85 on 2025-02-19
+// Edited line at 73 on 2025-03-15
 // Added line 69 on 2025-02-18
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,4 +1,3 @@
 // Edited line at 59 on 2025-02-28
 // Added line 94 on 2025-02-19
-// Edited line at 25 on 2025-03-13
 // Added line 85 on 2025-03-10
