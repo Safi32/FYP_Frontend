c1f95732cb05d75c9b941ff25c3202ab4dd5d712
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-18 10:00:00 +0500

    Improve form validation

--- src/components/Lorm.jsx
+++ src/components/Lorm.jsx
@@ -1,6 +1,5 @@
 // Edited line at 96 on 2025-03-12
 // Edited line at 55 on 2025-03-17
 // Added line 46 on 2025-01-21
-// Edited line at 61 on 2025-03-09
 // Edited line at 5 on 2025-03-04
 // Added line 55 on 2025-02-28
