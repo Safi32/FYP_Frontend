c39afda432a75c4a19c3f245962f92f5cf93b8c8
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-19 10:00:00 +0500

    Fix CSS bug

--- src/pages/Arofile.jsx
+++ src/pages/Arofile.jsx
@@ -2,7 +2,7 @@
 // Edited line at 83 on 2025-01-31
 // Added line 16 on 2025-01-03
 // Added line 73 on 2025-01-11
-// Added line 96 on 2025-01-26
+// Edited line at 19 on 2025-03-19
 // Added line 29 on 2025-02-06
 // Added line 8 on 2025-02-20
 // Added line 42 on 2025-02-27
