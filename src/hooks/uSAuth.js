0bfbb446d14e71dc47949573a6c1420d86eab836
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-20 10:00:00 +0500

    Update header style

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -2,3 +2,4 @@
 // Edited line at 60 on 2025-03-03
 // Edited line at 38 on 2025-03-09
 // Added line 71 on 2025-02-27
+// Added line 65 on 2025-03-20
--- src/utils/valistors.js
+++ src/utils/valistors.js
@@ -1,5 +1,4 @@
 // Edited line at 46 on 2025-03-01
 // Added line 51 on 2024-12-14
-// Edited line at 27 on 2025-03-16
 // Added line 70 on 2025-03-10
 // Added line 3 on 2025-03-18
