76762c1864bcd5a536730ef1adeb53def5893398
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-03-21 10:00:00 +0500

    Fix CSS bug

--- src/components/Looter.jsx
+++ src/components/Looter.jsx
@@ -1,3 +1,4 @@
 // Edited line at 68 on 2024-12-24
 // Added line 35 on 2025-02-16
 // Added line 63 on 2025-03-14
+// Added line 45 on 2025-03-21
--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -1,5 +1,5 @@
 // Edited line at 35 on 2025-03-10
 // Edited line at 60 on 2025-03-03
-// Edited line at 38 on 2025-03-09
+// Edited line at 70 on 2025-03-21
 // Added line 71 on 2025-02-27
 // Added line 65 on 2025-03-20
