752b97484e41ebd6e79e6bd9b8dcb25c8d7524f4
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-04-02 10:00:00 +0500

    Clean up code

--- src/hooks/uSAuth.js
+++ src/hooks/uSAuth.js
@@ -2,5 +2,5 @@
 // Added line 95 on 2025-01-25
 // Added line 43 on 2025-01-29
 // Edited line at 73 on 2025-03-15
-// Added line 69 on 2025-02-18
+// Edited line at 99 on 2025-04-02
 // Added line 56 on 2025-03-28
