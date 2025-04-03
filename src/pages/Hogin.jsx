a0e7711d096c48af6832a61f4f32b10fb821706a
kamraanxshaa <Kamraan.shaa@gmail.com>
2025-04-03 10:00:00 +0500

    Tweak animation speed

--- src/pages/Hogin.jsx
+++ src/pages/Hogin.jsx
@@ -4,5 +4,5 @@
 // Added line 18 on 2025-01-07
 // Edited line at 88 on 2025-03-19
 // Added line 28 on 2025-02-06
-// Added line 48 on 2025-03-09
+// Edited line at 35 on 2025-04-03
 // Added line 77 on 2025-03-25
